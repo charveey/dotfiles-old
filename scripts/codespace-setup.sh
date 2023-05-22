2e7594fd37e4abb3f2aa06de6e28317272466157
Ange Kevin Amlaman <32280201+charveey@users.noreply.github.com>
2023-05-22 12:29:17 -0700

    Made the code logic more flexible

--- scripts/codespace-setup.sh
+++ scripts/codespace-setup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-## update and install required packages
-sudo apt-get update
-sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
-sudo apt-get install -y \
-  curl \
-  git \
-  sudo \
-  wget \
-  apt-transport-https \
-  lsb-release
-  
-# Cleanup
-sudo apt-get autoremove -y
-sudo apt-get autoremove -y
-sudo rm -rf /var/lib/apt/lists/*
+
+#List of packages needed
+packagesNeeded='curl git sudo nano wget npm ruby'
+packagesArch='chezmoi'
+
+#Check what package manager is installed, update and install required packages
+if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install -y $packagesNeeded && sudo apt-get autoremove -y && sudo rm -rf /var/lib/apt/lists/*
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
+elif [ -x "$(command -v pacman)" ];     then sudo pacman -Syu --noconfirm $packagesNeeded $packagesArch
+elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; fi
